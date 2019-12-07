74cd5c42cc1d2c07601001cca7b01346a01ff99e
Lenard Frommelt <lenny09918050@googlemail.com>
2019-12-07 19:42:05 +0100

    adjusted recipe to be service-recipe

--- individualize.js
+++ individualize.js
@@ -2,5 +2,5 @@ const exec = require("child_process").exec
 const pathModule = require("path")
 
 var toolsetDir = pathModule.resolve(process.cwd(), "toolset")
-var scriptPath = pathModule.resolve(toolsetDir, "prepareThingyForMachine.pl")
-exec(scriptPath, {cwd:toolsetDir}, (err, out, stderr) => console.log(out + "\nindividualized machine!"))
+var scriptPath = pathModule.resolve(toolsetDir, "prepareThingyForService.pl")
+exec(scriptPath, {cwd:toolsetDir}, (err, out, stderr) => console.log(out + "\nindividualized service!"))
